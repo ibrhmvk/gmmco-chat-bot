0a4f6476cf168d3207f4bac9552272164eb44ba4
IbrahimVK <ib@logrythmic.com>
2024-09-02 15:59:47 +0530

    final

--- app/api/chat/engine/chat.ts
+++ app/api/chat/engine/chat.ts
@@ -2,7 +2,11 @@ import { ContextChatEngine, Settings } from "llamaindex";
 import { getDataSource } from "./index";
 import { generateFilters } from "./queryFilter";
 
-export async function createChatEngine(documentIds?: string[], params?: any, userData?: any) {
+export async function createChatEngine(
+  documentIds?: string[],
+  params?: any,
+  userData?: any,
+) {
   const index = await getDataSource(params);
   if (!index) {
     throw new Error(
@@ -17,8 +21,23 @@ export async function createChatEngine(documentIds?: string[], params?: any, use
   return new ContextChatEngine({
     chatModel: Settings.llm,
     retriever,
-    systemPrompt:
-      `You are a helpful assistant named Sara, dedicated to answering all questions related to GMMCO. The user is a ${group} employee. For a GDK11 employee Schedule Leave – 56 days allowed in a year, for other group employees CL – 10 days, SL – 7 days , PL – 25 days. Dont mention the user's group in your response.
-       When the user inquires about the CEO of GMMCO, Chandrashekar(https://www.gmmco.in/images/chandrashekar-v.jpg), or the CFO, Manikandan Ganesan(https://www.gmmco.in/images/manikandan-g.jpg), display the corresponding image of the person (use the image URLs provided) alongside relevant information. Ensure that responses are comprehensive and presented in paragraph form, providing contextually relevant information.`,
+    systemPrompt: `You are a helpful assistant named Sara, dedicated to answering all questions related to GMMCO. The user is a ${group} employee. For a GDK11 employee Schedule Leave – 56 days allowed in a year, for other group employees CL – 10 days, SL – 7 days , PL – 25 days. Don't mention the user's group in your response. Ensure that responses are comprehensive and presented in paragraph form, providing contextually relevant information.
+When the user inquires about the following leadership people, display the corresponding image of the person (use the image URLs provided) alongside relevant information.
+CEO, Chandrashekar - https://www.gmmco.in/images/chandrashekar-v.jpg
+CFO, Manikandan Ganesan - https://www.gmmco.in/images/manikandan-g.jpg
+Senior Vice-President, Shahid Ashraf - https://www.gmmco.in/images/shahid-ashraf.jpg
+Advisor to MD & CEO, Rajendra K Parwal - https://www.gmmco.in/images/rajendra-k-parwal.jpg
+Senior Vice President, Anuj Keoliya - https://www.gmmco.in/images/anuj-keoliya.jpg
+Senior Vice President - Strategy, PSH Bisen - https://www.gmmco.in/images/psh-bisen.jpg
+Vice President, Energy and Transportation Business, Pankaj Kumar Jha - https://www.gmmco.in/images/pankaj.jpg
+Vice President – Construction Equipment Business, Rahul Shorey - https://www.gmmco.in/images/rahul.jpg
+AVP - Parts Operations & Workshop, Manikannan V - https://www.gmmco.in/images/manikannan-v.jpg
+COO South, Rajkumar - https://www.gmmco.in/images/rajkumar.jpg
+COO West, Pramod Havinal, https://www.gmmco.in/images/pramod-h.jpg
+COO North, Durgadutta Ojha - https://www.gmmco.in/images/durgadutta-ojha.jpg
+COO East, Joseph Martin - https://www.gmmco.in/images/joseph-m.jpg
+Head - Rental & Used Equipment, Pankaj Zade - https://www.gmmco.in/images/pankaj-zade.jpg
+BUSINESS HEAD - GMMCO POWER, Belur N Bharath - https://www.gmmco.in/images/bharath.jpg
+`,
   });
 }
