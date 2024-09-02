a605944c58245c7353f3722629f90cc7f36797f1
IbrahimVK <ib@logrythmic.com>
2024-09-02 16:09:32 +0530

    fix

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,31 +1,47 @@
-"use client";
+'use client'
 
-import Header from "@/app/components/header";
-import ClientWrapper from "../components/ClientWrapper";
-import { supabase } from "../supabaseClient"; // Adjust the import path as needed
-import { useRouter } from "next/navigation";
-
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
+import Header from '@/app/components/header'
+import ClientWrapper from '../components/ClientWrapper'
+import { supabase } from '../supabaseClient' // Adjust the import path as needed
 
 export default function Home() {
-  const router = useRouter();
+  const router = useRouter()
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  useEffect(() => {
+    const checkLoginStatus = () => {
+      const token = localStorage.getItem(process.env.NEXT_PUBLIC_TOKEN_ID || '')
+      const userData = localStorage.getItem('user_data')
+      setIsLoggedIn(!!token && !!userData)
+    }
+
+    checkLoginStatus()
+  }, [])
 
   const handleLogout = async () => {
-    const { error } = await supabase.auth.signOut();
+    const { error } = await supabase.auth.signOut()
     if (!error) {
-      localStorage.removeItem(process.env.NEXT_PUBLIC_TOKEN_ID || "");
-      localStorage.removeItem("user_data");
-      router.push("/login");
+      localStorage.removeItem(process.env.NEXT_PUBLIC_TOKEN_ID || '')
+      localStorage.removeItem('user_data')
+      setIsLoggedIn(false)
+      router.push('/login')
     } else {
-      console.error("Error during sign out:", error);
+      console.error('Error during sign out:', error)
     }
-  };
+  }
+
+  if (!isLoggedIn) {
+    return null // Or a loading spinner
+  }
 
   return (
     <main className="h-screen w-screen flex justify-center items-center background-gradient">
       <div className="absolute top-4 right-4">
         <button
           onClick={handleLogout}
-          className=" text-black px-4 py-2 rounded font-sans font-bold"
+          className="text-black px-4 py-2 rounded font-sans font-bold"
         >
           Logout
         </button>
@@ -37,5 +53,5 @@ export default function Home() {
         </div>
       </div>
     </main>
-  );
+  )
 }
